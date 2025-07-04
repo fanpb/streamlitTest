eac18ac68d86ce8a291fe2652a025f77797ce549
fanpeibing <fanpeibing@swhysc.com>
2025-07-04 15:05:00 +0800

    【修改】

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
 st.title("The app title")
 
@@ -8,12 +6,3 @@ st.page_link("app.py", label="Home", icon="⚽️")
 st.page_link("pages/profile.py", label="My profile")
 
 st.write("Hello **world**!")
-
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3), 
-    columns=['a', 'b', 'c']
-    )
-
-st.write(chart_data)
-
-st.link_button('Click me', 'https://www.baidu.com')
